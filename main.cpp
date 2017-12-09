15dbd3a4049283353a2382914726aa8f31cbbc78
pythonCat <pythoncat@qq.com>
2017-12-09 18:05:09 +0800

    [feat]:complete the code of struct and arr[]

--- main.cpp
+++ main.cpp
@@ -1,5 +1,64 @@
 #include <iostream>
 
+using namespace std;
+const int SLEN = 30;
+struct student {
+    char fullName[SLEN];
+    char hobby[SLEN];
+    int oopLevel;
+};
+
+int getinfo(student pa[], int n);
+
+void display1(student st);
+
+void display2(student *st);
+
+void display3(const student pa[], int n);
+
+/*
+ * 补全程序
+ */
 int main() {
+    cout << "Enter class size: ";
+    int class_size;
+    cin >> class_size;
+    while (cin.get() != '\n')
+        continue;
+    student *ptr_stu = new student[class_size];
+
+    int entered = getinfo(ptr_stu, class_size);
+
+    for (int i = 0; i < entered; ++i) {
+        display1(ptr_stu[i]);
+        display2(&ptr_stu[i]);
+    }
+    display3(ptr_stu, entered);
+    delete[] ptr_stu;
+
+    cout << "Done\n";
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
