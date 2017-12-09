f1ba69f8136c2f92396c582d12302be224f7878a
pythonCat <pythoncat@qq.com>
2017-12-09 17:23:57 +0800

    [feat]:use vector replace array finish this task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <vector>
 /**
  * 程序清单7.15
  */
@@ -10,32 +11,32 @@ const std::array<std::string, Seasons> Snames =
         {"Spring", "Summer", "Fall", "Winter"};
 
 // function to modify array object
-void fill(double *pa, int size);
+void fill(std::vector<double> *da);
 
 // function that uses array object without modifying it
-void show(const double *da, int size);
+void show(std::vector<double> da);
 
 int main() {
-    double expenses[Seasons];
-    fill(expenses, Seasons);
-    show(expenses, Seasons);
+    std::vector<double> expenses(Seasons);
+    fill(&expenses);
+    show(expenses);
     return 0;
 }
 
-void fill(double *pa, int size) {
+void fill(std::vector<double> *da) {
     using namespace std;
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < da->size(); ++i) {
         cout << "Enter " << Snames[i] << " expenses ";
-        cin >> pa[i];
+        cin >> (*da)[i];
     }
 }
 
 
-void show(const double *da, int size) {
+void show(std::vector<double> da) {
     using namespace std;
     double total = 0.0;
     cout << "\nEXPENSES\n";
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < da.size(); ++i) {
         cout << Snames[i] << ": " << da[i] << endl;
         total += da[i];
     }
