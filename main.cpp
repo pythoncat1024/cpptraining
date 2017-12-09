cad9364bfcbfd93d67afca515a94410a0d6b51fe
pythonCat <pythoncat@qq.com>
2017-12-09 11:34:49 +0800

    [feat]: use struct as method paramater

--- main.cpp
+++ main.cpp
@@ -3,59 +3,56 @@
 
 
 using namespace std;
-const int Max = 10;
+struct box {
+    char marker[40];
+    float height;
+    float width;
+    float length;
+    float volume;
+};
 
-int input(double *arr); // 输入函数
-void show(const double *arr, int len); // 一行显示成绩
-double average(const double *arr, int len); // 报告平均成绩
-void dropInput(); // 丢弃多余输入
-/**
- 编写一个程序，要求用户输入最多10个高尔夫成绩，并将其存储在一个数组中。程序允许用户提早结束
- 输入，并在一行显示所有的成绩，然后报告平均成绩。请使用3个数组处理的函数来分别进行输入、显示
- 和计算平均成绩。
+void showBox(box obj);
+
+void setVolume(box *pBox);
+/*
+ 下面是一个结构的声明：
+struct box {
+    char marker[40];
+    float height;
+    float width;
+    float length;
+    float volume;
+};
+ a. 编写一个函数，按值传递box结构，并显示每个成员的值。
+ b. 编写一个函数，传递box结构的地址，并将volume成员设置为其他三维长度的乘积。
+ c. 编写一个使用这两个函数的简单程序。
  */
 int main(void) {
 
-    double arr[Max];
-    int size = input(arr);
-    show(arr, size);
-    double avg = average(arr, size);
-    cout << "avg==" << avg << endl;
-
+    box obj = {
+            "duck",
+            12.5f,
+            21.6f,
+            14.0f,
+            123.4f
+    };
+    showBox(obj);
+    setVolume(&obj);
+    showBox(obj);
     return 0;
 }
 
-double average(const double *arr, int size) {
-    double total = 0;
-    for (int i = 0; i < size; ++i) {
-        total += arr[i];
-    }
-    return total / size;
+void setVolume(box *pBox) {
+    pBox->volume = pBox->length * pBox->width * pBox->height;
 }
 
-void show(const double *arr, int size) {
-    cout << "arr(" << size << "):[";
-    for (int i = 0; i < size - 1; ++i) {
-        cout << arr[i] << ",";
-    }
-    cout << arr[size - 1] << "]" << endl;
-}
+void showBox(box obj) {
+    cout << "obj:{"
+         << obj.marker << ","
+         << obj.height << ","
+         << obj.width << ","
+         << obj.length << ","
+         << obj.volume << "}" << endl;
 
-int input(double *arr) {
-    int size = 0;
-    double element;
-    while (size < Max && (cin >> element) && element > 0) {
-        dropInput();
-        cout << "ele==" << element << endl;
-        *arr++ = element;
-        size++;
-    }
-    dropInput();
-    return size;
-}
 
-void dropInput() {
-    while (cin && cin.get() != '\n')continue;
-    cin.clear();
 }
-
