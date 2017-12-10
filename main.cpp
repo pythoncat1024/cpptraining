f4e00c401a6702a72ac637edff49439021c77ad0
pythonCat <pythoncat@qq.com>
2017-12-10 20:40:42 +0800

    [feat]: use typedef define method point type.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,10 @@ int main() {
 
 int (*pAdd)(int, int); // 定义函数指针 pAdd , pAdd不是一个函数，而是函数指针
 
+typedef int (*pFunAdd)(int, int); // 定义一个新的类型
+// pFunAdd 到底是什么类型呢？是一个函数指针类型，
+// 其中被指向的函数是参数为2个int,返回类型为int.
+
 int add(int x, int y);
 
 int main() {
@@ -22,6 +26,13 @@ int main() {
     int zz = (*pAdd)(3, 4);
 
     cout << "zz=" << zz << endl;
+    cout << "# # # # # # # # # # # # " << endl;
+
+    pFunAdd funAdd = add;
+
+    int q = funAdd(5, 6);
+
+    cout << "q =" << q << endl;
     return 0;
 }
 
