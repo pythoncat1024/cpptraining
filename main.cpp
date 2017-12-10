9ab38c9c6acd14a481272f6bf0f1f39e13ba83fe
pythonCat <pythoncat@qq.com>
2017-12-11 00:13:23 +0800

    [feat]:the diff about var between java and c++

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+struct student {
+    std::string name;
+    int age;
+};
 
 int main() {
+
+    string str;
+    unsigned long size = str.size(); // ok
+    cout << "str=" << str << " , size=" << size << " , " << sizeof(str) << endl;
+    cout << "# # # # # # # # # " << endl;
+
+    student kelly;
+    cout << kelly.name << " , " << kelly.age << sizeof(kelly) << endl; // ok
+    cout << "-- # # # # # # # # # " << endl;
+
+    student *rose = new student;
+    cout << (*rose).name << " , " << (*rose).age << sizeof(*rose) << endl;
+
+    delete rose;
     return 0;
 }
