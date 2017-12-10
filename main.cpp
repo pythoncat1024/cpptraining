bd8d66f7b71cd0bb283d5867e63543cbe88501b6
pythonCat <pythoncat@qq.com>
2017-12-10 20:35:27 +0800

    [feat]:easy method point define

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 
+/*
+char (*pFun)(int);
+
+char glFun(int a) { ; }
+
 int main() {
+    pFun = glFun;
+    (*pFun)(2);
     return 0;
+}*/
+
+
+int (*pAdd)(int, int); // 定义函数指针 pAdd , pAdd不是一个函数，而是函数指针
+
+int add(int x, int y);
+
+int main() {
+    using namespace std;
+    pAdd = add;
+    int zz = (*pAdd)(3, 4);
+
+    cout << "zz=" << zz << endl;
+    return 0;
+}
+
+int add(int x, int y) {
+    return x + y;
 }
