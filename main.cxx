9e77479d4d304b3fa38ad6f576eac3ececeffe70
pythonCat <pythoncat@qq.com>
2018-01-07 10:58:56 +0800

    [feat]:optimise enqueue() and dequeue() in Queue class

--- main.cxx
+++ main.cxx
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "Queue.h"
+
+void foo(que::Queue queue);
+
 using namespace que;
 using namespace std;
 
@@ -7,6 +10,7 @@ int main() {
 
     {
         Queue q1 = Queue(20);
+//        Queue q2 = Queue(6);
         cout << "q1=" << q1 << endl;
 
         Customer c1 = Customer();
@@ -15,24 +19,30 @@ int main() {
         c2.set(33);
 
         q1.enqueue(c1);
-        q1.enqueue(c2);
+        q1.dequeue();
         q1.enqueue(c2);
         q1.enqueue(c2);
         q1.enqueue(c1);
-        q1.enqueue(c2);
-        q1.enqueue(c2);
         cout << "X after enqueue q1=" << q1 << endl;
 
         q1.dequeue();
         cout << "YY after dequeue q1=" << q1 << endl;
 
-        q1.release();
+//        q1.release();
 
         cout << "zz after release q1=" << q1 << endl;
 //    cout << c1 << c2;
 
         int *arr = new int[1000];
-        cout << "zz after release q1=...." << q1 << endl;
+
+//        foo(q2);
+//        cout << "zz after release q1=...." << q1 << endl;
     }
     return 0;
 }
+
+//void foo(que::Queue queue) {
+//
+//    int size = queue.size();
+//    cout << "size==" << size<<endl;
+//}
