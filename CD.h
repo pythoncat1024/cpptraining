22930386bc58d61f0163cfd33212514e67b325a2
pythonCat <pythoncat@qq.com>
2018-01-14 11:07:10 +0800

    [feat]:CD and Classic

---
+++ CD.h
@@ -0,0 +1,46 @@
+//
+// Created by cat on 2018/1/14.
+//
+
+#ifndef CPP02_CD_H
+#define CPP02_CD_H
+
+// base class
+class CD { // represents a CD disk
+
+private:
+    char performers[50];
+    char label[20];
+    int selections; // number of selections
+    double playtime; // playing time in minutes
+public:
+    CD(char *performers, char *label, int selections, double playtime);
+
+    CD(const CD &cd);
+
+    CD();
+
+    virtual ~CD();
+
+    virtual void Report() const; // reports all CD data
+
+    CD &operator=(const CD &disk);
+};
+
+
+class Classic : public CD {
+private:
+    char primary[50];
+public:
+    Classic(char *primary, char *performers, char *label, int selections, double playtime);
+
+    Classic(const CD &cd, char *primary);
+
+    Classic();
+
+    virtual void Report() const override; // 这里的virtual 加不加都一样的
+
+    Classic &operator=(const Classic &disk);
+};
+
+#endif //CPP02_CD_H
