7c2581bdc611281b248a2968e7408322d1abe8c0
pythonCat <pythoncat@qq.com>
2018-01-19 22:35:56 +0800

    [feat]:queue 模板类的实现 init

---
+++ Queue.cxx
@@ -0,0 +1,5 @@
+//
+// Created by cat on 2018/1/19.
+//
+
+#include "Queue.h"
---
+++ Queue.h
@@ -0,0 +1,14 @@
+//
+// Created by cat on 2018/1/19.
+//
+
+#ifndef CPP02_QUEUE_H
+#define CPP02_QUEUE_H
+
+
+class Queue {
+
+};
+
+
+#endif //CPP02_QUEUE_H
